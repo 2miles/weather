c65c43fce76aec4af7f235348b3fa602cf479529
miles <miles4599@gmail.com>
2022-05-30 16:35:38 -0700

    moved parse_weather into functions.py. Fixed create_title()

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,11 @@ from .forms import CityForm
 import requests
 from datetime import datetime
 from environs import Env
-from .functions import get_coords_from_city, create_title_name
+from .functions import (
+    get_coords_from_city,
+    create_title_name,
+    parse_current_weather_data,
+)
 
 env = Env()
 env.read_env()
@@ -29,43 +33,24 @@ def home_page_view(request):
             pass
         return data
 
-    def build_current_weather_data_dict(data: dict) -> dict:
-        """
-        Takes all the data returned from the current_weather_data_from_coords() and
-        builds and formats a dict of specific key, values.
-        """
-        weather_data = {}
-        weather_data["name"] = data["name"]
-        weather_data["description"] = data["weather"][0]["description"].capitalize()
-        weather_data["icon"] = data["weather"][0]["icon"]
-        weather_data["temp"] = str(round(data["main"]["temp"])) + " F"
-        weather_data["humidity"] = str(data["main"]["humidity"]) + "%"
-        weather_data["windspeed"] = str(round(data["wind"]["speed"])) + " mph"
-        weather_data["sunrise"] = datetime.fromtimestamp(
-            data["sys"]["sunrise"] + data["timezone"]
-        ).strftime("%I:%M %p")
-        weather_data["sunset"] = datetime.fromtimestamp(
-            data["sys"]["sunset"] + data["timezone"]
-        ).strftime("%I:%M %p")
-        return weather_data
-
     if request.method == "POST":
         form = CityForm(request.POST)
         if form.is_valid():
             form.save()
-    form = CityForm()
+    else:
+        form = CityForm()
     cities = City.objects.all()
     weather_data_list = []
+    data = {}
     for city in cities:
-        data = None
         weather_data = None
         coords = get_coords_from_city(API_KEY, city.name, city.country, city.state)
         data = get_current_weather_data_from_coords(
             API_KEY, "imperial", coords[0], coords[1]
         )
-        weather_data = build_current_weather_data_dict(data)
+        weather_data = parse_current_weather_data(data)
         weather_data["printed_name"] = create_title_name(
-            city.name, weather_data["name"]
+            str(city), weather_data["name"]
         )
         weather_data_list.append(weather_data)
 
