e326330465f3c2948fb9c47afc71874b6ddf58c0
miles <miles4599@gmail.com>
2022-05-30 16:36:39 -0700

    updated form.py clean method

--- weather/forms.py
+++ weather/forms.py
@@ -23,9 +23,41 @@ class CityForm(ModelForm):
             ),
         }
 
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        name = name.title()
+        return name
+
+    def clean_state(self):
+        if "state" in self.cleaned_data:
+            state = self.cleaned_data["state"]
+            state = state.upper()
+        return state
+
+    def clean_country(self):
+        if "country" in self.cleaned_data:
+            country = self.cleaned_data["country"]
+            country = country.upper()
+        if self.cleaned_data["state"] != "":
+            country = "US"
+        return country
+
     def clean(self):
         cleaned_data = super(ModelForm, self).clean()
+        if cleaned_data["country"] == "":
+            existing_city_count = City.objects.filter(name=cleaned_data["name"]).count()
+            print(cleaned_data)
+        else:
+            existing_city_count = (
+                City.objects.filter(name=cleaned_data["name"])
+                .filter(state=cleaned_data["state"])
+                .filter(country=cleaned_data["country"])
+                .count()
+            )
+        print(existing_city_count)
+        if existing_city_count > 0:
+            raise ValidationError("That Location already exists in the database!")
         coords = get_coords_from_city(API_KEY, **cleaned_data)
         if coords == None:
-            raise ValidationError("Not a valid location")
+            raise ValidationError("Not a valid location!")
         return cleaned_data
--- weather/functions.py
+++ weather/functions.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 
 def get_coords_from_city(API_key, name, country="", state="") -> list:
@@ -24,10 +25,36 @@ def get_coords_from_city(API_key, name, country="", state="") -> list:
     return coords
 
 
+def parse_current_weather_data(data: dict) -> dict:
+    """
+    Takes all the data returned from the current_weather_data_from_coords() and
+    builds and formats a dict of specific key, values. If input dict is None, return None
+    """
+    if data == None:
+        return None
+    weather_data = {}
+    weather_data["name"] = data["name"]
+    weather_data["description"] = data["weather"][0]["description"].capitalize()
+    weather_data["icon"] = data["weather"][0]["icon"]
+    weather_data["temp"] = str(round(data["main"]["temp"])) + " F"
+    weather_data["humidity"] = str(data["main"]["humidity"]) + "%"
+    weather_data["windspeed"] = str(round(data["wind"]["speed"])) + " mph"
+    weather_data["sunrise"] = datetime.fromtimestamp(
+        data["sys"]["sunrise"] + data["timezone"]
+    ).strftime("%I:%M %p")
+    weather_data["sunset"] = datetime.fromtimestamp(
+        data["sys"]["sunset"] + data["timezone"]
+    ).strftime("%I:%M %p")
+    return weather_data
+
+
 def create_title_name(str1, str2) -> str:
     """
     Appends str2, in parentheses, to str1.
     """
-    if str1.lower() != str2.lower():
-        return (str1 + " (" + str2 + ")").title()
-    return str2.title()
+    parts = str1.split(",")
+    first_part = parts[0].strip()
+    print(first_part.lower(), str2.lower())
+    if first_part.lower() != str2.lower():
+        return str1 + " (" + str2 + ")"
+    return str1
