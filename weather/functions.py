e21f06ad7ce4df39c46eac3c3c8e93b45bd1052a
miles <miles4599@gmail.com>
2022-05-30 02:08:48 -0700

    moved get_coords to its own file

---
+++ weather/functions.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def get_coords_from_city(API_key, name, country="", state="") -> list:
+    """
+    Makes an API call for a givin cities lattitude and longitude.
+    state_code is optional for non-US locations.
+    Returns a list of the respective coords.  If the city is not found return None.
+    """
+    coords = []
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={name},{state},{country}&limit=1&appid={API_key}"
+    res = requests.get(url)
+    try:
+        res.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        return "Error: " + str(e)
+    data = res.json()
+    try:
+        lat, lon = data[0]["lat"], data[0]["lon"]
+    except:
+        return None
+    coords.append(lat)
+    coords.append(lon)
+    return coords
