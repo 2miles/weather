f8d64f9a9acba970cacf1565acaf07755e3ba633
miles <miles4599@gmail.com>
2022-05-30 02:09:39 -0700

    added clean method to form to check if location exists

--- weather/forms.py
+++ weather/forms.py
@@ -1,13 +1,31 @@
-from dataclasses import field
-from pyexpat import model
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput, ValidationError
 from .models import City
+from .functions import get_coords_from_city
+from environs import Env
+
+env = Env()
+env.read_env()
+
+API_KEY = env.str("API_KEY")
 
 
 class CityForm(ModelForm):
     class Meta:
         model = City
-        fields = ["name"]
+        fields = ["name", "state", "country"]
         widgets = {
-            "name": TextInput(attrs={"class": "input", "placeholder": "City Name"})
+            "name": TextInput(attrs={"class": "input", "placeholder": "Detroit"}),
+            "state": TextInput(
+                attrs={"class": "input", "placeholder": "MI (optional)"}
+            ),
+            "country": TextInput(
+                attrs={"class": "input", "placeholder": "US (optional)"}
+            ),
         }
+
+    def clean(self):
+        cleaned_data = super(ModelForm, self).clean()
+        coords = get_coords_from_city(API_KEY, **cleaned_data)
+        if coords == None:
+            raise ValidationError("Not a valid location")
+        return cleaned_data
